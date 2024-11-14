2fd9dbb90a680087115208005fe09e33f31e797a
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-14 11:39:37 -0300

    feat: create class desenvolvedor

---
+++ dsus.js
@@ -0,0 +1,9 @@
+class Desenvolvedor {
+    constructor (nome, idDesenvolvedor, idade, contato, plataforma) {
+        this.nome = nome;
+        this.idDesenvolvedor = idDesenvolvedor;
+        this.idade = idade;
+        this.contato = contato;
+        this.plataforma = plataforma;
+    }
+}
