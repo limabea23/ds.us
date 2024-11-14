f764fb2ccf78f9d97d44a89378d1793fd7261ee9
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-14 11:44:14 -0300

    feat: create class missoes

--- dsus.js
+++ dsus.js
@@ -18,3 +18,11 @@ class Aluno {
     }
 }
 
+class Missoes {
+    constructor (nomeMissao, idMissao, explicacao, pontuacao) {
+        this.nomeMissao = nomeMissao;
+        this.idMissao = idMissao;
+        this.explicacao = explicacao;
+        this.pontuacao = pontuacao;
+    }
+}
