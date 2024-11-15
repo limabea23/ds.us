6f4d75e693cb9e67862ba245c152ae9709982e14
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-14 22:14:51 -0300

    refactor: class missoes

---
+++ models/missoes.js
@@ -0,0 +1,8 @@
+class Missoes {
+    constructor (nomeMissao, idMissao, explicacao, pontuacao) {
+        this.nomeMissao = nomeMissao;
+        this.idMissao = idMissao;
+        this.explicacao = explicacao;
+        this.pontuacao = pontuacao;
+    }
+}
