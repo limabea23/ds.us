3d6909c1dc89924d0fd34279bfd408819efb3651
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-14 22:12:18 -0300

    refactor: class aluno

---
+++ models/aluno.js
@@ -0,0 +1,9 @@
+class Aluno {
+    constructor (nomeAluno, idPlayer, idade, online, impostor) {
+        this.nomeAluno = nomeAluno;
+        this.idPlayer = idPlayer;
+        this.idade = idade;
+        this.online = online;
+        this.impostor = impostor;
+    }
+}
