8bcbcfb6138f67e1d875b2a97c425a6f755e6864
Beatriz L <beatrizlima.tds1.senai@gmail.com>
2024-11-14 22:08:39 -0300

    refactor: class desenvolvedor

---
+++ models/desenvolvedor.js
@@ -0,0 +1,9 @@
+class Desenvolvedor {
+    constructor (nomeDev, idDesenvolvedor, idade, contato, plataforma) {
+        this.nomeDev = nomeDev;
+        this.idDesenvolvedor = idDesenvolvedor;
+        this.idade = idade;
+        this.contato = contato;
+        this.plataforma = plataforma;
+    }
+}
